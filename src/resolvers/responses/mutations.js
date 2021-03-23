c682d12cdf8bc8cd89a03851348deb26f090e3a8
jherbas <jhonnyherbasv@gmail.com>
2021-03-23 10:29:53 -0400

    task mutations: implement response resolver mutations.

--- src/resolvers/responses/mutations.js
+++ src/resolvers/responses/mutations.js
@@ -2,8 +2,49 @@ import { ServerData } from '../../data/serverData.js';
 const serverData = new ServerData();
 
 const responseMutations = {
-  createResponse: (parent, args, context, info) => {
-    return "Response created successfully!";
+  // create response
+  createResponse: async (parent, args, context, info) => {
+    let body = {
+      name: args.name,
+      response: args.response
+    };
+    // create response data
+    let res = await serverData.post(body, 'responses');
+    return (res.status == 201) ? "Response created successfully!" : 
+      "Error " + res.status + " , Message : " + res.message;
+  },
+  //update response
+  updateResponse: async (parent, args, context, info) => {
+    let url = 'responses/' + args.responseId;
+    let body = {
+      name: args.name,
+      response: args.response
+    };
+    // update response data
+    let res = await serverData.put(body, url);
+    return (res.status == 200) ? "Response updated successfully!" : 
+      "Error " + res.status + " , Message : " + res.message;
+  },
+  // remove response
+  removeResponse: async (parent, args, context, info) => {
+    let url = 'responses/' + args.responseId;
+    // remove response data
+    let res = await serverData.delete({}, url);
+    return (res.status == 200) ? "Response removed successfully!" : 
+      "Error " + res.status + " , Message : " + res.message;
+  },
+  // assign response to api
+  assignResponseToApi: async (parent, args, context, info) => {
+    let url = 'responses/' + args.responseId;
+    let body = {
+      api_id: args.apiId,
+      path: args.path,
+      method: args.method
+    };
+    // assign response to api
+    let res = await serverData.post(body, url);
+    return (res.status == 201) ? "Response assigned to Api successfully!" : 
+      "Error " + res.status + " , Message : " + res.message;
   }
 };
 
--- src/types/responses/mutations.js
+++ src/types/responses/mutations.js
@@ -1,7 +1,10 @@
 // Response Mutations
 
 const responseMutations = `
-  createResponse(name: String!, response: JSON!): String!
+  createResponse(name: String!, response: JSON!): String
+  updateResponse(responseId: String!, name: String!, response: JSON!): String
+  removeResponse(responseId: String!): String
+  assignResponseToApi(responseId: String!, apiId: String!, path: String!, method: String!): String
 `;
 
 export {responseMutations};
