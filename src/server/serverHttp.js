710efe7ac2539a1f762c1543ff12729a28dba3be
Gutierrez, Andres <andru1236@gmail.com>
2021-03-24 14:58:45 -0400

    add GQL_PORT to env
    Signed-off-by: Gutierrez, Andres <andru1236@gmail.com>

--- src/server/serverHttp.js
+++ src/server/serverHttp.js
@@ -2,15 +2,16 @@ import express from "express";
 import { ApolloServer } from "apollo-server-express";
 
 import { apolloSchema } from "./gqlSchemas.js";
-import { logger } from "../infrastructure";
+import { logger, readEnv } from "../infrastructure";
+
+readEnv();
 
 const server = new ApolloServer(apolloSchema);
 const app = express();
 server.applyMiddleware({ app });
 
 export const start = () => {
-  
-  app.listen({ port: 4000 }, () =>
+  app.listen({ port: process.env.GQL_PORT }, () =>
     logger.debug("Now browse to http://localhost:4000" + server.graphqlPath)
   );
 };
