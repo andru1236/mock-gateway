1f3ee6573f268edbc4f7b88bc1efd81718ec1c1d
jherbas <jhonnyherbasv@gmail.com>
2021-03-25 16:18:36 -0400

    task refactoring : implement mutation resolvers using api bridge rest server.

--- src/resolvers/apis/apiTranslator.js
+++ src/resolvers/apis/apiTranslator.js
@@ -1,6 +1,68 @@
 export default {
-    createApi: {
-        mockApiPath: "/apis",
-        method: "POST",
-    }
+  createApi: () => {
+    return {
+      apiPath: "/apis",
+      method: "POST",
+    };
+  },
+  updateApi: (apiId) => {
+    return {
+      apiPath: `/apis/${apiId}`,
+      method: "PUT",
+    };
+  },
+  removeApi: (apiId) => {
+    return {
+      apiPath: `/apis/${apiId}`,
+      method: "DELETE",
+    };
+  },
+  startApi: (apiId) => {
+    return {
+      apiPath: `/apis/${apiId}/start`,
+      method: "POST",
+    };
+  },
+  stopApi: (apiId) => {
+    return {
+      apiPath: `/apis/${apiId}/stop`,
+      method: "POST",
+    };
+  },
+  createRoute: (apiId) => {
+    return {
+      apiPath: `/apis/${apiId}/routes`,
+      method: "POST",
+    };
+  },
+  updateRoute: (apiId) => {
+    return {
+      apiPath: `/apis/${apiId}/routes`,
+      method: "PUT",
+    };
+  },
+  removeRoute: (apiId) => {
+    return {
+      apiPath: `/apis/${apiId}/routes`,
+      method: "DELETE",
+    };
+  },
+  createRouteParams: (apiId, routeId) => {
+    return {
+      apiPath: `/apis/${apiId}/routes/${routeId}/params`,
+      method: "POST",
+    };
+  },
+  updateRouteParams: (apiId, routeId) => {
+    return {
+      apiPath: `/apis/${apiId}/routes/${routeId}/params`,
+      method: "PUT",
+    };
+  },
+  removeRouteParams: (apiId, routeId) => {
+    return {
+      apiPath: `/apis/${apiId}/routes/${routeId}/params`,
+      method: "DELETE",
+    };
+  },
 }
