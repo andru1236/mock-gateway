0524b396485d9df6e78c098dd24f0a4bf400e689
jherbas <jhonnyherbasv@gmail.com>
2021-03-28 21:55:59 -0400

    task refactoring : update error handler messages.

---
+++ src/infrastructure/errors/errorInfo.js
@@ -0,0 +1,27 @@
+export const errorInfo = {
+  success: {
+    "POST": { status: 200, message: "Post data successfully." },
+    "PUT": { status: 200, message: "Put data successfully." },
+    "DELETE": { status: 200, message: "Delete data successfully."},
+  },
+  dbcServerError: {
+    "POST": { status: 500, message: "Error on dbc server trying to Post data." },
+    "PUT": { status: 500, message: "Error on dbc server trying to Put data." },
+    "DELETE": { status: 500, message: "Error on dbc server trying to Delete data." },
+  },
+  responseError: {
+    "POST": { status: 400, message: "Something went wrong when trying to Post data." },
+    "PUT": { status: 400, message: "Something went wrong when trying to Put data." },
+    "DELETE": { status: 400, message: "Something went wrong when trying to Delete data." },
+  },
+  nonFound: {
+    "404": { status: 404, message: "Error: Data no found." },
+  },
+  defaultSuccess: {
+    "200": { status: 200, message: "Process executed successfully." },
+    "201": { status: 201, message: "Process executed successfully." },
+  },
+  defaultError: {
+    "400": { status: 400, message: "Something went wrong, try again." },
+  }
+};
