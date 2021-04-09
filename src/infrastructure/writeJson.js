01216e495e70a79db833038834bd56f6fe696650
jherbas <jhonnyherbasv@gmail.com>
2021-04-08 21:40:46 -0400

    refactor task: implement write Json content component.

---
+++ src/infrastructure/writeJson.js
@@ -0,0 +1,25 @@
+import { errorHandler, errors, errorInfo } from "./errors";
+import { logger } from "./logger";
+import WriteJsonFile from "write-json-file";
+import readEnv from "./readEnv";
+readEnv();
+const EXPORTED_JSON_PATH = process.env.EXPORTED_JSON_RESPONSES_PATH;
+
+export const writeJsonFile = async (fileNamePath, content) => {
+  try {
+    if (typeof content !== "object" || content === null || content === undefined) {
+      throw new errors.ResponseError(errorInfo['jsonError']['emptyJson'].message);
+    }
+
+    const responseFilePath = `${EXPORTED_JSON_PATH}${fileNamePath}`;
+    console.log(responseFilePath);
+    if (responseFilePath === '' || responseFilePath === null || responseFilePath === undefined) {
+      throw new errors.ResponseError(errorInfo['jsonError']['fileName'].message);
+    }
+
+    await WriteJsonFile(responseFilePath, content);    
+    return errorInfo['defaultSuccess']['200'].message;
+  } catch (error) {
+    return errorHandler(error);
+  }
+};
