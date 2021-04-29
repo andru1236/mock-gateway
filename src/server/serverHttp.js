6721a453bba8e6b478d550882ddad5040596c371
Gutierrez, Andres <andres.gutierrez@sciencelogic.com>
2021-04-29 17:22:18 -0400

    Add size for the request body

--- src/server/serverHttp.js
+++ src/server/serverHttp.js
@@ -1,4 +1,5 @@
 import express from "express";
+import bodyParser from "body-parser";
 import { ApolloServer } from "apollo-server-express";
 
 import { apolloSchema } from "./gqlSchemas.js";
@@ -8,10 +9,12 @@ readEnv();
 
 const server = new ApolloServer(apolloSchema);
 const app = express();
+app.use(bodyParser.json({limit: '50mb'}));
 server.applyMiddleware({ app });
 
+
 export const start = () => {
-  app.listen({ port: process.env.GQL_PORT }, () =>
+  app.listen({port: process.env.GQL_PORT}, () =>
     logger.debug(
       `GQL running over to http://localhost:${process.env.GQL_PORT}` +
         server.graphqlPath
